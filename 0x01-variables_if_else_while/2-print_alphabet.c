ab422d4fd7c68ba7dceaffe4c4594a03f6122ec5
Okeke Chidera <okekechidera98@gmail.com>
2022-09-14 20:09:20 +0000

    add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * Return: 0
+ *
+ */
+
+int main(void) 
+{
+	char lc;
+
+	/* lc : lowerr case */
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
+	
