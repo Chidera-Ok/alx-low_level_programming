0e5f4069e5150ee71d03726e2586a85e7489af82
Okeke Chidera <okekechidera98@gmail.com>
2022-09-15 11:44:35 +0000

    make corrections to 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,20 +8,18 @@
 
 int main(void)
 {
-	char num;
+	int num;
 
 	for (num = '0'; num <= '9'; num++)
 	{
+		putchar(num);
+
 		if (num != '9')
 		{
-			putchar(num);
 			putchar(',');
-		}
-		else
-		{
-			putchar(num);
-			putchar('\n');
+			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
