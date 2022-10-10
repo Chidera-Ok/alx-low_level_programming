4f1e41880c7ece52444561350152c7bd759ab6db
Okeke Chidera <okekechidera98@gmail.com>
2022-10-10 20:48:02 +0000

    add 1-pi.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef OBJECT_LIKE_MACRO
-#define OBJECT_LKE_MACRO
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LKE_MACRO_H
 
 #define SIZE 1024
 
-#endif /* OBJECT_LKE_MACRO */
+#endif /* OBJECT_LKE_MACRO_H */
